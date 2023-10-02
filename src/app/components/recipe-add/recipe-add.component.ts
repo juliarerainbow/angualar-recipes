bbefe406e6efda8ecc6f05218b31a11db903537c
labstraction <45163263+labstraction@users.noreply.github.com>
2023-10-02 13:21:51 +0200

    starting form

--- src/app/components/recipe-add/recipe-add.component.ts
+++ src/app/components/recipe-add/recipe-add.component.ts
@@ -2,34 +2,34 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataService } from 'src/app/services/data.service';
 import { Recipe } from 'src/app/model/recipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-add',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './recipe-add.component.html',
   styleUrls: ['./recipe-add.component.scss']
 })
 export class RecipeAddComponent {
 
+  newRecipe:Recipe = {
+    name: '',
+    createdAt: 0,
+    ingredients: [],
+    description: '',
+    category: 0,
+    url: ''
+  };
+
   constructor(private dataServ: DataService){}
 
   submitRecipe(){
-    console.log('mi hai premuto')
-    const newRecipe: Recipe = {
-      name: 'cacio e pepe',
-      createdAt: Date.now(),
-      ingredients: [
-        'pecorino romano',
-        'pepe',
-        'spaghetti',
-        'olio EVO'
-      ],
-      description: 'grattuggiare il pecorino...',
-      category: 1,
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Spaghetti_cacio_e_pepe_%28creamy_as_it_should_be%29.jpg/520px-Spaghetti_cacio_e_pepe_%28creamy_as_it_should_be%29.jpg',
-    }
-    this.dataServ.postRecipe(newRecipe).subscribe(addedRecipe => console.log(addedRecipe));
+    console.log(this.newRecipe)
+    //this.dataServ.postRecipe(this.newRecipe).subscribe(addedRecipe => console.log(addedRecipe));
   }
 
 }
